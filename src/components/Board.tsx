5561d87d15a7652fda70d1b2251a797fb3ce3caf
ShawnCone <deandre.dave@gmail.com>
2023-05-12 15:57:28 -0700

    overlay done

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -15,6 +15,17 @@ export default function Board() {
     typeof questionSchema
   >>(null);
 
+  // Get question info with side effects
+  function getQuestionInfo(title: string) {
+    setQuestionInfo({
+      title: title,
+      question: "What country is this?",
+      imageLink:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Italian_regions_provinces_white_no_labels.svg/1200px-Italian_regions_provinces_white_no_labels.svg.png",
+      answer: "Italy",
+    });
+  }
+
   return (
     <>
       {questionInfo !== null && (
@@ -31,7 +42,112 @@ export default function Board() {
 
       <div className="flex h-full w-full flex-grow flex-col items-center justify-center px-5 pt-3">
         <div className="text-3xl">Board</div>
-        <div className="mt-10">
+        <div className="relative mt-10">
+          <button
+            className="relative left-8 top-[269px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("GEO 101");
+            }}
+          />
+          <button
+            className="relative left-[166px] top-[269px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("MATH 101");
+            }}
+          />
+
+          <button
+            className="relative left-[300px] top-[180px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("PE 201");
+            }}
+          />
+          <button
+            className="relative left-[280px] top-[330px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("CHEM 201");
+            }}
+          />
+          <button
+            className="relative left-[575px] top-[330px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("SEX 201");
+            }}
+          />
+          <button
+            className="relative left-[773px] top-[335px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("???");
+            }}
+          />
+          <button
+            className="relative left-[755px] top-[500px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("STAT 201");
+            }}
+          />
+          <button
+            className="relative left-[980px] top-[500px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("BIO 301");
+            }}
+          />
+          <button
+            className="relative left-[1230px] top-[251px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("GEN 301");
+            }}
+          />
+          <button
+            className="relative left-[1370px] top-[251px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("ART 401");
+            }}
+          />
+          <button
+            className="relative left-[295px] top-[180px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("ENG 201");
+            }}
+          />
+
+          <button
+            className="relative left-[435px] top-[180px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("HIST 201");
+            }}
+          />
+          <button
+            className="relative left-[625px] top-[65px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("HIST 201");
+            }}
+          />
+          <button
+            className="relative left-[760px] top-[65px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("ECON 201");
+            }}
+          />
+          <button
+            className="relative left-[740px] top-[180px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("GEO 301");
+            }}
+          />
+          <button
+            className="relative left-[873px] top-[180px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("PHYS 301");
+            }}
+          />
+          <button
+            className="relative left-[860px] top-[55px] h-5 w-5 rounded bg-blue-600 opacity-50"
+            onClick={() => {
+              getQuestionInfo("CS 301");
+            }}
+          />
+
           <Image src={boardImage} height={900} width={1600} alt="board image" />
         </div>
         <div className="flex w-full items-center justify-start gap-2">
